606ce3fa26d515929c89c9a557702ccc5b475e45
abigailarmijohernandez <abi1521@hotmail.com>
2025-11-25 21:27:23 -0600

    feat: add new Angular client application with chatbot functionality and update server routes.

---
+++ packages/client/appAI/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { Chatbot } from './chatbot/chatbot';
+
+export const routes: Routes = [
+    { path: '', component: Chatbot },
+];
---
+++ packages/client/appAI/src/app/chatbot/chatbot.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HlmTextareaImports } from '@spartan-ng/helm/textarea';
+import { HlmButton } from '@spartan-ng/helm/button';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { HlmIcon } from '@spartan-ng/helm/icon';
+import { lucideArrowUp } from '@ng-icons/lucide';
+
+@Component({
+    selector: 'app-chatbot',
+    imports: [HlmTextareaImports,
+        HlmButton,
+        NgIcon,
+        HlmIcon],
+    providers: [
+        provideIcons({ lucideArrowUp }),
+    ],
+    templateUrl: './chatbot.html',
+    styleUrl: './chatbot.css',
+})
+export class Chatbot {
+
+}
